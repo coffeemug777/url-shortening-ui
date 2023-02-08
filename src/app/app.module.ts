d233a49544d7b0daa603e4e34caa0ae2c7aff814
Denny Wibowo <indocoffee@gmail.com>
2023-02-08 13:56:19 -0500

    Add angular material indigo blue, global typography and enable animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { UrlReportComponent } from './component/url-report/url-report.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, UrlReportComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
